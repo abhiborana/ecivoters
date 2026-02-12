5913870303a3d56da435ae1cbe1701f7da71a852
Abhishek Borana <123462968+abhiborana@users.noreply.github.com>
2026-02-12 10:11:26 +0530

    Modify navbar title to indicate legacy version
    Updated the navbar title to include '(legacy)' for clarity.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,10 +6,11 @@ const Navbar = () => {
     <header className="w-full p-2 border-b">
       <Link href={"/"} className="flex items-center gap-2">
         <Image src={"/logo.svg"} alt="ECI Voters" width={40} height={40} />
-        <span className="font-bold text-lg">ECI Voters Finder</span>
+        <span className="font-bold text-lg">ECI Voters Finder (legacy)</span>
       </Link>
     </header>
   );
 };
 
 export default Navbar;
+
